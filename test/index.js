609cea71d9692346f606620ea124652c4c0a3655
Andrew Grathwohl <andrew@grathwohl.me>
2016-07-06 13:03:44 -0400

    fix test so that it works again

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
+/* jshint node:true */
+/* jshint esversion:6 */
 'use strict';
 
 const b = require("../");
 
-const a = b.calculate('./test.mp4', function(c) {
+const a = b.calculate('./test.mp4', function(err, c) {
     setTimeout(function () {
         console.log("Actual: " + c);
     }, 1000);
