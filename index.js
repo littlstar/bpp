308d91b156747e5260d621a8c317bae716e61660
Andrew Grathwohl <andrew@grathwohl.me>
2016-07-06 10:04:06 -0400

    actually make the code work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfprobePath(process.env.FFPROBE || '/usr/bin/ffprobe');
 ffmpeg.setFfmpegPath(process.env.FFMPEG || '/usr/bin/ffmpeg');
 
-exports.calculate = function calcBpp (videoFile) {
+exports.calculate = function calcBpp (videoFile, cb) {
     ffmpeg.ffprobe(videoFile, function(err, metadata) {
         const height = metadata.streams[0].height;
         const width  = metadata.streams[0].width;
@@ -18,7 +18,6 @@ exports.calculate = function calcBpp (videoFile) {
         const bits   = metadata.streams[0].bit_rate;
         const fps = parseFloat(fpsVal[0]) / parseFloat(fpsVal[1]);
         const bpp = bits / (height * width * fps);
-        console.log('Bits per pixel: ' + bpp.toFixed(3));
-        return bpp;
+        cb(bpp);
     });
 };
