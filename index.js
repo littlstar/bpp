7a64b6be9ccbb585a89904e8f3d99beaca1dd059
Joseph Werle <jwerle@users.noreply.github.com>
2016-07-06 12:25:34 -0400

    Make node callback friendly and catch errors
    This commit makes the callback given to the `calculate()` function node friendly.
    This commit also wraps the `ffmpeg.ffprobe()` function in a  `try/catch` expression
    to catch errors thrown in that function and propagate them to the callback.

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfprobePath(process.env.FFPROBE || '/usr/bin/ffprobe');
 ffmpeg.setFfmpegPath(process.env.FFMPEG || '/usr/bin/ffmpeg');
 
-exports.calculate = function calcBpp (videoFile, cb) {
-    ffmpeg.ffprobe(videoFile, function(err, metadata) {
+exports.calculate = function calculate (videoFile, cb) {
+    try { ffmpeg.ffprobe(videoFile, onprobe); }
+    catch (err) { return cb(err); }
+    function onprobe(err, metadata) {
+        if (err) { return cb(err); }
         const height = metadata.streams[0].height;
         const width  = metadata.streams[0].width;
         const fpsVal = metadata.streams[0].r_frame_rate.split('/');
         const bits   = metadata.streams[0].bit_rate;
         const fps = parseFloat(fpsVal[0]) / parseFloat(fpsVal[1]);
         const bpp = bits / (height * width * fps);
-        cb(bpp);
-    });
+        cb(null, bpp);
+    }
 };
